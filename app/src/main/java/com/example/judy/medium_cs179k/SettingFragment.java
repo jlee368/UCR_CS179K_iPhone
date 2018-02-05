020a203aa0f4d108cae85228cb10eb12e3a81060
jlee368 <jlee368@ucr.edu>
2018-02-05 14:08:52 -0800

    Connectdd layouts to drawer

---
+++ app/src/main/java/com/example/judy/medium_cs179k/SettingFragment.java
@@ -0,0 +1,23 @@
+package com.example.judy.medium_cs179k;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Judy Lee on 2/4/2018.
+ */
+
+public class SettingFragment extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.setting_layout,container, false);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        return myView;
+    }
+}
